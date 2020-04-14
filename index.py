c130b3cdfcb74703a63ee60a1c2153e028484bc5
claranceatgalvanize <claranceatgalvanize@gmail.com>
2020-04-13 23:59:44 -0700

    Variables and Datatypes Exercise

--- index.py
+++ index.py
@@ -20,40 +20,52 @@
 
 # Data Types
 
-failed_subjects = 2
-name = 'John'
+# failed_subjects = 2
+# name = 'John'
 
-print('Dear Mrs Badger')
-print('Your son ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
-print(name + ' will need to redo ' + str(failed_subjects) + ' courses.')
-name = 'Eric'
-print(name + ' is doing well in geography.')
+# print('Dear Mrs Badger')
+# print('Your son ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
+# print(name + ' will need to redo ' + str(failed_subjects) + ' courses.')
+# name = 'Eric'
+# print(name + ' is doing well in geography.')
 
-print(type('hello'))
-print(type(1))
-print(type(1.64))
-print(type(True))
+# print(type('hello'))
+# print(type(1))
+# print(type(1.64))
+# print(type(True))
 
-a = int(1)
-b = int(2.5)
-c = int("3")
-d= int(float("3.4"))
-e = float(1)
-f = float(2.5)
-g = float('3')
-h = float('4.23')
-i = str(22)
-j = str(3.01)
+# a = int(1)
+# b = int(2.5)
+# c = int("3")
+# d= int(float("3.4"))
+# e = float(1)
+# f = float(2.5)
+# g = float('3')
+# h = float('4.23')
+# i = str(22)
+# j = str(3.01)
 
-print({
-    "a should be 1": a,
-    'b should be 2': b,
-    'c should be 3': c,
-    'd should be 3': d,
-    'e should be 1.0': e,
-    'f should be 2.5': f,
-    'g should be 3.0': g,
-    'h should be 4.23': h,
-    'i should be "22"': i,
-    'j should be "3.01"': j
-})
+# print({
+#     "a should be 1": a,
+#     'b should be 2': b,
+#     'c should be 3': c,
+#     'd should be 3': d,
+#     'e should be 1.0': e,
+#     'f should be 2.5': f,
+#     'g should be 3.0': g,
+#     'h should be 4.23': h,
+#     'i should be "22"': i,
+#     'j should be "3.01"': j
+# })
+
+# Variables and Datatypes Exercise
+
+#Create appropriate variables for items name, the price and hom many you have in stock.
+
+itme_name = 'bulb'
+price = 9.99
+inventory = 120
+is_in_inventory = True
+
+print(itme_name,price,inventory, is_in_inventory)
+
