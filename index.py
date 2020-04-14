9b3828282157f972995a6ebf58060387ee442788
claranceatgalvanize <claranceatgalvanize@gmail.com>
2020-04-13 23:19:26 -0700

    Wroking with variables."

--- index.py
+++ index.py
@@ -1,5 +1,18 @@
-print('welcom to python 102!')
-print('welcom to python 101!')
-print('Create hammer')
-print('Create nails')
-print('Use hammer and nails')
+# Using the print command in python.
+
+# print('welcom to python 102!')
+# print('welcom to python 101!')
+# print('Create hammer')
+# print('Create nails')
+# print('Use hammer and nails')
+
+# Variables
+
+failed_subjects = '2'
+name = 'John'
+
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name + ' will need to redo ' + failed_subjects + ' courses.')
+name = 'Eric'
+print(name + ' is doing well in geography.')
